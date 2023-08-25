b0565c1e0e1771413ea064a0bd061c517cae3de0
Jason Schneck <jasonschneck@Jasons-Mac-mini.local>
2023-08-25 11:27:18 -0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/birdmagic.png',
+    title: '80s',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/jordan.png',
+    title: '90s',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/kobe.png',
+    title: '2000s',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/bronJ.png',
+    title: '2010s',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/giannis.png',
+    title: '2020s',
+  },
+];
+
+export const startingFeatures = [
+  'Find a NBA world that interests you the most and explore',
+  'Read directions and have enough space on the outside',
+  'No need to beat around the bush, just stay on the court and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Connecting',
+    subtitle:
+        'Connect with NBA Metaverse fans all across the world',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'Making the world more realistic, experiencing the world you chose',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/wemby.png',
+    title: 'How can Wembanyama have an early impact during his first couple games',
+    subtitle:
+        'Victor Wembanyama has not even stepped foot on a NBA court and already has the hype of a first-ballot hall of famer.',
+  },
+  {
+    imgUrl: '/ant.png',
+    title: 'Can Anthony Edwards take another leap, solidifying himself as a star',
+    subtitle:
+        'After leading his team to the playoffs, and being team USAs best player this summer, can he take yet another leap to becoming a top 10 player this year.',
+  },
+  {
+    imgUrl: '/book.jpg',
+    title: 'Which team is poised for the title this year',
+    subtitle:
+        'The Nuggets look to defend their title, as teams like the Suns and Celtics made some serious acquisitions to put themselves into a prime situation to compete',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
